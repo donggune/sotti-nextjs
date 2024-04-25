ad50bfde0426fe985d81c081cd9cebc4cc24a0f3
lion <rhkwkdkrl@gmail.com>
2024-04-25 23:17:27 +0900

    새로운 화면

---
+++ app/create-account/page.tsx
@@ -0,0 +1,31 @@
+import FormInput from "@/components/form-input";
+import { ChatBubbleOvalLeftEllipsisIcon } from "@heroicons/react/24/solid";
+import Link from "next/link";
+import React from "react";
+
+export default function CreateAccount() {
+  return (
+    <div className="flex flex-col gap-10 py-8 px-6">
+      <div className="flex flex-col gap-2 *:font-medium">
+        <h1 className="text-2xl">안녕하세요!</h1>
+        <h2 className="text-xl">Fill in the form below to create your account!</h2>
+      </div>
+      <form className="flex flex-col gap-3">
+        <FormInput type="text" placeholder="Username" required errors={[]} />
+        <FormInput type="email" placeholder="Email" required errors={[]} />
+        <FormInput type="password" placeholder="Password" required errors={[]} />
+        <FormInput type="password" placeholder="Password Confirm" required errors={[]} />
+        <button className="primary-btn h-10">Create account</button>
+      </form>
+      <div className="w-full h-px bg-neutral-500" />
+      <div>
+        <Link className="primary-btn flex h-10 items-center justify-center gap-3" href="/sms">
+          <span>
+            <ChatBubbleOvalLeftEllipsisIcon className="h-6 w-6" />
+          </span>
+          <span>Sign up with SMS</span>
+        </Link>
+      </div>
+    </div>
+  );
+}
---
+++ components/form-input.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+// type 설정할 때 type를 쓰든 interface 를 사용하든 취향차이임. 아무거나 해도됨
+interface Props {
+  type: string;
+  placeholder: string;
+  required: boolean;
+  errors: string[];
+}
+
+export default function FormInput({ type, placeholder, required, errors }: Props) {
+  return (
+    <div className="flex flex-col gap-2">
+      <input
+        className="bg-transparent rounded-md w-full h-10 focus:outline-none ring-1 focus:ring-2 ring-neutral-200 focus:ring-sotti-main border-none placeholder:text-neutral-400"
+        type={type}
+        placeholder={placeholder}
+        required={required}
+      />
+      {errors.map((error, index) => (
+        <span key={index} className="text-red-500 font-medium">
+          {error}
+        </span>
+      ))}
+    </div>
+  );
+}
