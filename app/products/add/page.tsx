2648217859e80df3dc88ee2525c2154a43417bd6
lion <rhkwkdkrl@gmail.com>
2024-05-26 22:01:05 +0900

    상품등록 form

--- app/products/add/page.tsx
+++ app/products/add/page.tsx
@@ -4,21 +4,45 @@ import Button from "@/components/button";
 import Input from "@/components/input";
 import { useState } from "react";
 import { FaImage } from "react-icons/fa";
+import { uploadProduct } from "./actions";
 
 export default function AddProduct() {
   const [preview, setPreview] = useState("");
-  const onImageChange = () => {};
+  const onImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { files },
+    } = event;
+
+    if (!files) {
+      return;
+    }
+
+    const file = files[0];
+
+    if (!file) {
+      setPreview("");
+      return;
+    }
+
+    const url = URL.createObjectURL(file);
+    setPreview(url);
+  };
   return (
     <div>
-      <form className="flex flex-col gap-4 p-5">
+      <form action={uploadProduct} className="flex flex-col gap-4 p-5">
         <label
           htmlFor="photo"
-          className="flex flex-col items-center justify-center border-2 aspect-square rounded-md border-neutral-400 border-dashed cursor-pointer"
+          className="flex flex-col items-center justify-center border-2 aspect-square rounded-md border-neutral-400 border-dashed cursor-pointer bg-cover"
+          style={{ backgroundImage: `url(${preview})` }}
         >
-          <span>
-            <FaImage className="text-gray-400 size-44" />
-          </span>
-          <span className="text-sm text-gray-400">사진을 추가해주세요.</span>
+          {preview === "" ? (
+            <>
+              <span>
+                <FaImage className="text-gray-400 size-44" />
+              </span>
+              <span className="text-sm text-gray-400">사진을 추가해주세요.</span>
+            </>
+          ) : null}
         </label>
         <Input onChange={onImageChange} type="file" name="photo" id="photo" className="hidden" />
         <Input type="text" name="title" placeholder="제목" />
